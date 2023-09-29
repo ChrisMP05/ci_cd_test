6db1c3ddbc11f9f3e6994587ea1539a44a53d7c3
Christian Montoya <usachris.10@gmail.com>
2023-09-28 23:10:58 -0500

    pruebaCorreo6

--- enviarCorreo.ps1
+++ enviarCorreo.ps1
@@ -1,11 +1,28 @@
 # Configura los detalles del correo electrónico
+# $SmtpServer = "smtp.example.com"
+# $SmtpPort = 587
+# $SmtpUser = "usachris.10@email.com"
+# $SmtpPassword = "plemquezbhekrxjy"
+# $Recipient = "usachris@email.com"
+# $Subject = "Notificación de éxito del flujo de trabajo de GitHub Actions"
+# $Body = "El flujo de trabajo se ejecutó correctamente en GitHub Actions."
+
+# # Envía el correo electrónico
+# Send-MailMessage -SmtpServer $SmtpServer -Port $SmtpPort -UseSsl -Credential (Get-Credential -UserName $SmtpUser -Password $SmtpPassword) -From $SmtpUser -To $Recipient -Subject $Subject -Body $Body
+
+# Configura los detalles del correo electrónico
 $SmtpServer = "smtp.example.com"
 $SmtpPort = 587
 $SmtpUser = "usachris.10@email.com"
-$SmtpPassword = "plemquezbhekrxjy"
-$Recipient = "usachris@email.com"
-$Subject = "Notificación de éxito del flujo de trabajo de GitHub Actions"
+
+# Solicita la contraseña de manera interactiva
+$SecurePassword = Read-Host -AsSecureString -Prompt "Ingresa la contraseña para $SmtpUser"
+
+# Crea las credenciales
+$Credentials = New-Object System.Management.Automation.PSCredential ($SmtpUser, $SecurePassword)
+
+# Cuerpo del mensaje
 $Body = "El flujo de trabajo se ejecutó correctamente en GitHub Actions."
 
 # Envía el correo electrónico
-Send-MailMessage -SmtpServer $SmtpServer -Port $SmtpPort -UseSsl -Credential (Get-Credential -UserName $SmtpUser -Password $SmtpPassword) -From $SmtpUser -To $Recipient -Subject $Subject -Body $Body
+Send-MailMessage -SmtpServer $SmtpServer -Port $SmtpPort -UseSsl -Credential $Credentials -From $SmtpUser -To $Recipient -Subject $Subject -Body $Body
